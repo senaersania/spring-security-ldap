0447773134ee7745c4a3a5216e68090dce0ba09d
senaersania <senaersania@yahoo.com>
2022-01-04 15:40:41 +0700

    [TEST] - spring-security-ldap-hirearchy-structure

---
+++ src/test/java/com/javavelis/springsecurityldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javavelis.springsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
